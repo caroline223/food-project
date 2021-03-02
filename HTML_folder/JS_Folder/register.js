237ede5790f76d5dafb5c33dfe04ebd36d9d6cbb
caroline223 <carolineforrester22@gmail.com>
2021-03-01 23:41:13 -0500

    updating the html

--- HTML_folder/JS_Folder/register.js
+++
@@ -1,9 +0,0 @@
-// Confirm sumbission about form
-
-var form = document.getElementById('registerForm');
-    form.addEventListener("submit",function() {
-        alert("Thank you for submitting this form. We will contact you soon.");
-        console.log("The form has been submitted!");
-    });
-        
-  
